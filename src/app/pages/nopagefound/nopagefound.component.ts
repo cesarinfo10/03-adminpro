e5169ea26ee15a1e3583cfccb20abab835e6d0eb
cesarinfo10 <32378721+cesarinfo10@users.noreply.github.com>
2022-08-16 21:01:18 -0300

    fin seccion 3 diseño

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.css']
+})
+export class NopagefoundComponent  {
+
+  year = new Date().getFullYear();
+
+}
